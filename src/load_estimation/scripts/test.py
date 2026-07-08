5da878148ab8b51c802cd9c874c747a44fd97a5c
Phattanarat <phattanarat.jeedjeen@gmail.com>
2026-07-08 20:49:24 +0700

    stable load mass

--- src/load_estimation/scripts/test.py
+++ src/load_estimation/scripts/test.py
@@ -2,7 +2,8 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float64MultiArray       
+from std_msgs.msg import Float64MultiArray  
+from rcl_interfaces.msg import ParameterDescriptor    
 from collections import deque
 from load_estimation_interfaces.msg import Debug
 import numpy as np
@@ -10,6 +11,11 @@ import numpy as np
 class LoadEstimation(Node):
     def __init__(self):
         super().__init__('load_estimate')
+
+        self.declare_parameter('use_filter', True)
+        self.declare_parameter('use_offset', True)
+        self.use_filter = self.get_parameter('use_filter').get_parameter_value().bool_value
+        self.use_offset = self.get_parameter('use_offset').get_parameter_value().bool_value
 
         # linkage length (m)
         self.Ldg = 2494.4/1000
@@ -27,6 +33,7 @@ class LoadEstimation(Node):
         self.Lgi = np.sqrt(self.LgiY**2 + self.LgiX**2)
         self.Lgh = 1630/1000
 
+        # number of lift cylinders
         self.n = 2.0 
 
         # angle (rad)
@@ -41,14 +48,14 @@ class LoadEstimation(Node):
         self.Ar = 0.014;    # piston cross-section area
 
         # measured
-        self.theta_g = None # AGO (rad)
-        self.theta_t = None # FEC (rad)
-        self.Pb = None      # bottom cylinder pressure (Pa)
-        self.Pr = None      # rod side pressure (Pa)
-        self.raw_Pb = None
-        self.raw_Pr = None
-        self.offset_Pb = None
-        self.offset_Pr = 380.0
+        self.theta_g = 0.0    # AGO (rad)
+        self.theta_t = 0.0   # FEC (rad)
+        self.Pb = None       # bottom cylinder pressure (Pa)
+        self.Pr = None       # rod side pressure (Pa)
+        self.raw_Pb = 0.0
+        self.raw_Pr = 0.0
+        self.offset_Pb = 0.0
+        self.offset_Pr = 0.0
         self.median_Pb = None
         self.median_Pr = None
 
@@ -59,10 +66,6 @@ class LoadEstimation(Node):
         self.window_size = 50
         self.r_history = deque(maxlen=self.window_size)
         self.b_history = deque(maxlen=self.window_size)
-
-        # tuned
-        self.LloadG = 0.0
-        self.HloadG = 0.0
 
         hz = 100
         self.create_timer(1/hz, self.load_estimate)
@@ -83,7 +86,7 @@ class LoadEstimation(Node):
 
         self.publisher = self.create_publisher(
             Debug, 
-            "/debug_geometry", 
+            "/debug", 
             10
         )
 
@@ -91,123 +94,60 @@ class LoadEstimation(Node):
         self.raw_b = msg.data[0]
         self.raw_r = msg.data[1]
 
-        # median filter
-        self.b_history.append(self.raw_b)
-        self.r_history.append(self.raw_r)
+        if self.use_filter:
+            # median filter
+            self.b_history.append(self.raw_b)
+            self.r_history.append(self.raw_r)
 
-        if len(self.b_history) < self.window_size:
-            return
+            if len(self.b_history) < self.window_size:
+                return
 
-        self.median_Pb = np.median(self.b_history)
-        self.median_Pr = np.median(self.r_history)
+            self.median_Pb = np.median(self.b_history)
+            self.median_Pr = np.median(self.r_history)
+            Pb = self.median_Pb
+            Pr = self.median_Pr
+
+        else:
+            Pb = self.raw_b
+            Pr = self.raw_r
 
         # offset cal
-        if self.theta_g is not None:
-            self.offset_Pb = 4225 + 1979*(self.theta_g - self.theta_g_offset) + -534*(self.theta_g - self.theta_g_offset)**2
+        if self.use_offset:
+            self.offset_Pb = 4225 + 1979*self.theta_g + -534*self.theta_g**2
+            self.offset_Pr = 350
 
-            # sensor_val2Pa, pure_load pressure
-            self.Pb = np.maximum(0.0,(self.median_Pb - self.offset_Pb) * (40*10**6)/32767)
-            self.Pr = np.maximum(0.0,(self.median_Pr - self.offset_Pr) * (40*10**6)/32767)
+        self.Pb = np.maximum(0.0,(Pb - self.offset_Pb)) * (40*10**6)/32767
+        self.Pr = np.maximum(0.0,(Pr - self.offset_Pr)) * (40*10**6)/32767
 
     def angle_callback(self, msg:Float64MultiArray):
-        self.theta_g = msg.data[0] + self.theta_g_offset
-        self.theta_t = msg.data[1] + self.theta_t_offset
+        self.theta_g = msg.data[0]
+        self.theta_t = msg.data[1]
+
 
     def load_estimate(self):
         if self.theta_g is None or self.theta_t is None or self.Pb is None or self.Pr is None:
             return
         
-        # eq 15
-        FGD = self.FGO + self.theta_g + self.DGA
-        Ldf = np.sqrt(self.Lfg**2 + self.Ldg**2 - 2*self.Lfg*self.Ldg*np.cos(FGD))
-
-        # eq 16
-        Laf = np.sqrt(self.Lag**2 + self.Lfg**2 - 2*self.Lag*self.Lfg*np.cos(self.theta_g - self.FGO))
-
-        # add
-        DEF = self.theta_t + self.DEC
-        EFD = np.arcsin(self.Lde * np.sin(DEF) / Ldf)
-        EDF = np.pi - EFD - DEF
-
-        # tilt prismatic length
-        Lef = self.Lde * np.sin(EDF) / np.sin(EFD)
-
-        # eq 17
-        a3 = self.Ldg * np.sin(self.theta_g + self.DGA) - self.LfgY
-        a4 = self.Ldg * np.cos(self.theta_g + self.DGA) - self.LfgX
-        a5 = Ldf**2 + Lef**2 - self.Lde**2
-        a6 = 2 * Ldf * Lef
-        theta_f = np.arctan2(a3, a4) + np.arccos(a5/a6)
-
-        # eq 18
-        theta_e = -(np.pi - self.DEC - self.theta_t)
-
-        # eq 19
-        a9 = self.Lad**2 + self.Ldg**2 - self.Lag**2
-        a10 = 2 * self.Lad * self.Ldg
-        ADC = np.arccos(a9/a10) + EDF - np.pi - self.theta_d
-
-        # eq 20
-        Lac = np.sqrt(self.Lad**2 + self.Lcd**2 - 2*self.Lad*self.Lcd*np.cos(ADC))
-
-        # eq 21
-        a13 = Lac**2 + self.Lbc**2 - self.Lab**2
-        a14 = 2 * Lac * self.Lbc
-        a15 = self.Lcd**2 + Lac**2 - self.Lad**2
-        a16 = 2 * self.Lcd * Lac
-        theta_c = np.pi + np.arccos(a13/a14) - np.arccos(a15/a16)
-
-        # eq 22
-        a17 = self.Lab**2 + self.Lbc**2 - Lac**2
-        a18 = 2 * self.Lab * self.Lbc
-        theta_b = np.arccos(a17/a18) - np.pi
-
-        # lift prismatic length
-        Lih = np.sqrt(self.Lgh**2 + self.Lgi**2 - 2*self.Lgh*self.Lgi*np.cos(self.theta_g + self.IGO))
-
-        GIH = np.arcsin((self.Lgh/Lih) * np.sin(self.theta_g + self.IGO))
-
-        # eq 14
-        Hbmcyl = self.IGO - GIH
+        theta_g = self.theta_g + self.theta_g_offset
+        Lih = np.sqrt(self.Lgh**2 + self.Lgi**2 - 2*self.Lgh*self.Lgi*np.cos(theta_g + self.IGO))  
+        GIH = np.arcsin((self.Lgh/Lih) * np.sin(theta_g + self.IGO))
+        Hbmcyl = np.pi - self.IGO - GIH
         theta_i = np.pi - (self.IGO + Hbmcyl)
-
-        a = self.LloadG * np.cos(self.theta_g + theta_c + self.theta_d + theta_e - theta_b + 180 - self.HloadG)
-        # eq 8
-        b = -self.Lab * np.sin(theta_b)
-        # eq 9 
-        c = self.Lcd * np.sin(theta_c)
-        # eq 10
-        d = -self.Lde * np.sin(theta_e)
-        # eq 11
-        e = self.Lfg * np.sin(np.pi - theta_f + self.FGO)
-        # eq 12
         f = self.Lgi * np.sin(theta_i)
-        # eq 13
-        Lw = self.Lag * np.cos(self.theta_g) + a
-        # force applied to lift arm cylinders
+        Lw = self.Lag * np.cos(theta_g) 
         Fc = self.n * (self.Ab * self.Pb - self.Ar * self.Pr)  
-        W = (Fc*f / (Lw - (a*c*e/b*d)))/9.81
+        W = (Fc*f/Lw)/9.81
 
         msg = Debug()
-        msg.a = a
-        msg.b = b
-        msg.c = c
-        msg.d = d
-        msg.e = e
+        msg.theta_i = theta_i
+        msg.theta_g = self.theta_g
+        msg.pb = self.Pb
+        msg.pr = self.Pr
         msg.f = f
         msg.lw = Lw
         msg.fc = Fc
-        msg.w = W 
-        msg.raw_pb = self.raw_b
-        msg.raw_pr = self.raw_r  
-        msg.offset_pb = self.offset_Pb
-        msg.offset_pr = self.offset_Pr
-        msg.median_pb = self.median_Pb
-        msg.median_pr = self.median_Pr
-        msg.pb = self.Pb
-        msg.pr = self.Pr
-        msg.theta_g = self.theta_g
-        msg.theta_t = self.theta_t
+        msg.w = W
+        
         self.publisher.publish(msg)
 
 def main(args=None):
